1dd939ac83d0f8dff1921e60c1089e16294e05ca
Scott Cavazos <scavazos1@buffs.wtamu.edu>
2023-08-30 20:47:43 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using lab2_starter.Models;
+
+Widget widgetOne = new Widget();
+widgetOne.Name = "Weather";
+Console.WriteLine($"widgetOne.Name = {widgetOne.Name}");
+
+Widget widgetTwo = new Widget();
+widgetTwo.Name = "News";
+Console.WriteLine($"widgetTwo.Name = {widgetTwo.Name}");
---
+++ Widget.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace lab2_starter.Models
+{
+    public class Widget
+    {
+        public int WidgetId {get; set;}
+        public string? Name {get; set;}
+    }
+}
